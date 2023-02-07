71afee58ce62eb6abccecbc7ce8aa671428796bc
Viacheslav Liashenko <87814580+vliashenko@users.noreply.github.com>
2023-02-07 20:30:28 +0200

    Add files via upload

--- 25+5clock/src/App.jsx
+++ 25+5clock/src/App.jsx
@@ -1,78 +1,91 @@
-import { useState, useEffect } from 'react';
-
-const getMMSS = (time) => {
-  let ms = time * 60 * 1000;
-  let minutes = Math.floor(ms / 60000);
-  let seconds = ((ms % 60000) / 1000).toFixed(0);
-
-  return (minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-}
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
 
 const App = () => {
-  const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(1);
-  const [timeLeft, setTimeLeft] = useState(getMMSS(sessionLength));
-  const [timeLabel, setTimeLabel] = useState('Session');
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [intervalId, setIntervalId] = useState('');
+  const [displayTime, setDisplayTime] = useState(1500);
+  const [breakTime, setBreakTime] = useState(5);
+  const [sessionTime, setSessionTime] = useState(25);
+  const [timerOn, setTimerOn] = useState(false);
+  const [timerId, setTimerId] = useState("Session");
+  const audioElement = useRef(document.getElementById('beep'));
+  let loop = undefined;
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    return `${minutes}:${seconds}`;
+  };
+
+  const changeTime = (amount, type) => {
+    let newCount;
+    if (type === "Session") {
+      newCount = sessionTime + amount;
+    } else {
+      newCount = breakTime + amount;
+    }
+
+    if (newCount > 0 && newCount <= 60 && !timerOn) {
+      type === "Session" ? setSessionTime(newCount) : setBreakTime(newCount);
+      if (type === "Session") {
+        setDisplayTime(newCount * 60);
+      }
+    }
+  };
+
+  const setActive = () => {
+    setTimerOn(!timerOn);
+  };
 
   useEffect(() => {
-    if(timeLeft === '00:00') {
-      clearInterval(intervalId);
-    }
-  },[timeLeft])
-  
-  const startTimer = (length) => {
-    let value = length;
-    let ms = value * 60 * 1000;
-    
-    if(!isPlaying && getMMSS(length) !== timeLeft) {
-      ms = ( Number(timeLeft.split(':')[0])*60 + Number(timeLeft.split(':')[1]) ) * 1000;
-    }
-
-    if(!isPlaying) {
-      setIsPlaying(true);
-    
-      const id = setInterval(() => {
-        ms = ms - 1000
-        let minutes = Math.floor(ms / 60000);
-        let seconds = ((ms % 60000) / 1000).toFixed(0);
-        
-        setTimeLeft((minutes < 10 ? '0' : '') + minutes + ":" + (seconds < 10 ? '0' : '') + seconds);
+    if (timerOn && displayTime > 0) {
+      const interval = setInterval(() => {
+        setDisplayTime(displayTime - 1);
       }, 1000);
+      return () => clearInterval(interval);
+    } else if (displayTime === 0) {
+      audioElement.current.play();
+      audioElement.current.currentTime = 0;
 
-      setIntervalId(id)
-    } else {
-      clearInterval(intervalId);
-      setIsPlaying(false);
-      setIntervalId('');
+      if (timerId === "Session") {
+        setDisplayTime(breakTime * 60);
+        setTimerId("Break");
+      }
+      if (timerId === "Break") {
+        setDisplayTime(sessionTime * 60);
+        setTimerId("Session");
+      }
     }
-  }
+  }, [breakTime, sessionTime, displayTime, timerId, timerOn]);
 
-  const resetTimer = () => {
-    setBreakLength(5);
-    setSessionLength(25);
-    setTimeLeft('25:00');
-    setTimeLabel('Session');
-    setIsPlaying(false);
-    clearInterval(intervalId);
-  }
+  const resetTime = () => {
+    setBreakTime(5);
+    setSessionTime(25);
+    setDisplayTime(1500);
+    setTimerId("Session");
+    setTimerOn(false);
+    clearInterval(loop);
+    audioElement.current.load();
+  };
 
   return (
     <div className="app">
       <div id="clock">
-        <h3>25 + 5 Clock</h3>
+        <h3 className="main-title">25 + 5 Clock</h3>
         <div className="controls">
           <div className="break-container">
             <h5 id="break-label">Break Length</h5>
             <div className="btn-group">
-              <button id="break-increment" className="break-arrow-up">
+              <button onClick={() => changeTime(1, "Break")} id="break-increment" className="break-arrow-up">
                 <i className="fa-solid fa-circle-up"></i>
               </button>
               <p id="break-length">
-                {breakLength}
+                {breakTime}
               </p>
-              <button id="break-decrement" className="break-arrow-down">
+              <button onClick={() => changeTime(-1, "Break")} id="break-decrement" className="break-arrow-down">
                 <i className="fa-solid fa-circle-down"></i>
               </button>
             </div>
@@ -80,13 +93,13 @@ const App = () => {
           <div className="session-container">
             <h5 id="session-label">Session Length</h5>
             <div className="btn-group">
-              <button id="session-increment" className="session-arrow-up">
+              <button onClick={() => changeTime(1, "Session")} id="session-increment" className="session-arrow-up">
                 <i className="fa-solid fa-circle-up"></i>
               </button>
               <p id="session-length">
-                {sessionLength}
+                {sessionTime}
               </p>
-              <button id="session-decrement" className="session-arrow-down">
+              <button onClick={() => changeTime(-1, "Session")} id="session-decrement" className="session-arrow-down">
                 <i className="fa-solid fa-circle-down"></i>
               </button>
             </div>
@@ -94,19 +107,19 @@ const App = () => {
         </div>
         <div className="timer">
           <h4 id="timer-label">
-            Session
+            {timerId}
           </h4>
           <h1 id="time-left">
-            {timeLeft}
+            {formatTime(displayTime)}
           </h1>
-        </div>
-        <div className="main-controls">
-          <button onClick={() => startTimer(sessionLength)} id="start_stop">
-            <i className="fa-solid fa-play"></i>
-          </button>
-          <button onClick={resetTimer} id="reset">
-            <i className="fa-solid fa-arrow-rotate-left"></i>
-          </button>
+          <div className="main-controls">
+            <button onClick={setActive} id="start_stop">
+              <i className={`fa-solid fa-${timerOn ? 'pause' : 'play'}`}></i>
+            </button>
+            <button onClick={resetTime} id="reset">
+              <i className="fa-solid fa-arrow-rotate-left"></i>
+            </button>
+          </div>
         </div>
       </div>
     </div>
